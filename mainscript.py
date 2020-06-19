025b4a45bd7e8b747cc1690fb82cbe6a10222080
adriancort <42118321+adriancort@users.noreply.github.com>
2020-06-19 18:55:20 +0200

    Actually Plots Things First Time

--- mainscript.py
+++ mainscript.py
@@ -1,8 +1,8 @@
 # Offline Imports
 from utils import oneDayDelay
-import utils
+from utils import oneDayDelay_plt
 # Other Libraries, may require PIP installation
-import statistics as stat
+import statistics as st
 import matplotlib.pyplot as plt
 from pandas_datareader import data, wb
 from datetime import datetime
@@ -26,7 +26,9 @@ for number in range(0, len(tickersOfInterest)):
 start_date = '2005-01-01'
 end_date = date.today()
 
-print('\n \n**************** \n DOWNLOADING... \n**************** \n \n')
+print('\n \n********************')
+print(' DOWNLOADING ...')
+print('******************** \n \n')
 
 stock_data = {}
 for i in range(len(tickerStr)):
@@ -35,8 +37,6 @@ for i in range(len(tickerStr)):
         tickerStr[i], start_date, end_date)}
     stock_data.update(case)
 
-# More advanced versions can rely on the .get() method to find the key (Ticker)
-
 # Moving through all the data to find pct_changes
 stock_data_returns = {}
 for i in range(len(stock_data.keys())):
@@ -44,7 +44,10 @@ for i in range(len(stock_data.keys())):
     stock_data_returns.update(case)
 
 # Row Delay Correlation
-print('\n \n******************** \n ALL DATA DOWNLOADED \n******************** \n \n')
+print('\n \n********************')
+print(' ALL DATA DOWNLOADED')
+print('******************** \n \n')
 
 testcols = stock_data_returns['ASML']['Close']
 testdictionary = oneDayDelay(testcols)
+oneDayDelay_plt(testdictionary[0], testdictionary[1])
--- utils.py
+++ utils.py
@@ -4,11 +4,22 @@ from datetime import datetime
 import pandas as pd
 import numpy as np
 import datetime
+import statistics as st
 from datetime import date
 
 
-def oneDayDelay(stock, minTranche=-0.2, maxTranche=0.2, numTranche=20):
+# Selects number of singificant digits
+def signif(x, p):
+    x = np.asarray(x)
+    x_positive = np.where(np.isfinite(x) & (x != 0), np.abs(x), 10**(p - 1))
+    mags = 10 ** (p - 1 - np.floor(np.log10(x_positive)))
+    return np.round(x * mags) / mags
+
+
+# Evaluates "given yesterday's fall, whats the expected probability of a rise?"
+def oneDayDelay(stock, minTranche=-0.2, maxTranche=0.2, numTranche=10):
     q = np.linspace(minTranche, maxTranche, numTranche)
+    q = signif(q, 3)
     prb_dict = {}
     for i in range(len(q)):
         empty_List = {q[i]: ([])}
@@ -21,4 +32,30 @@ def oneDayDelay(stock, minTranche=-0.2, maxTranche=0.2, numTranche=20):
                 tranchefound = True
                 prb_dict[q[ii]].append(stock[i + 1])
             ii += 1
-    return prb_dict
+    rev_list = list(reversed(range(len(prb_dict.keys()))))
+    for i in rev_list:  # eliminates keys with less than 1 datapoints
+        if len(prb_dict[q[i]]) <= 1:
+            del prb_dict[q[i]]  # eleminates key
+            q = np.delete(q, i, 0)  # eliminates array element
+
+    return prb_dict, q
+
+
+def oneDayDelay_plt(stock_dict, q):
+    index = []
+    mean_return = []
+    for i in q:
+        index.append(i)
+        mean_return.append(st.mean(stock_dict[i]))
+    df3 = pd.DataFrame({'Mean Return': mean_return}, index=index)
+    df3.plot.bar(rot=0, grid=True).legend(
+        loc='center left', bbox_to_anchor=(1.0, 0.5))
+    plt.show()
+
+
+def pos(lst):
+    return [x for x in lst if x >= 0] or None
+
+
+def neg(lst):
+    return [x for x in lst if x < 0] or None
